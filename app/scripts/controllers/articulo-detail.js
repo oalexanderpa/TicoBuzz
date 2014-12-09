c1f1abdeb1722c78a2615e4f7ef9d6b5228af7a2
Andres <andres_m113@hotmail.com>
2014-12-09 02:23:59 -0600

    Fix socialbar
    Ahora el socialbar esta sticky en diferentes resoluciones

--- app/scripts/controllers/articulo-detail.js
+++ app/scripts/controllers/articulo-detail.js
@@ -14,13 +14,19 @@ angular.module('ticoBuzzApp')
   .directive('scroll', function ($window) {
     return function(scope) {
         angular.element($window).bind('scroll', function() {
-             if (this.pageYOffset >= 220) {
-             	 angular.element('#sharebar').addClass('sharebar sharebar-scrolled');
-                 console.log('Scrolled below header.');
-             } else {
-             	 angular.element('#sharebar').removeClass('sharebar-scrolled');
-                 console.log('Header is in view.');
-             }
+            console.log(angular.element('#stats').offset().top+32);
+            if (this.pageYOffset >= angular.element('#stats').offset().top+32) {
+             	  angular.element('#sharebar').addClass('sharebar sharebar-scrolled');
+                angular.element('#artImg').addClass('articuloIndividual-Imagen');
+                console.log('Scrolled below header.');
+            } else {
+             	  angular.element('#sharebar').removeClass('sharebar-scrolled');
+                angular.element('#artImg').removeClass('articuloIndividual-Imagen');
+                console.log('Header is in view.');
+            }
+            angular.element('#sharebar').css({
+                width:  angular.element('#bodyLeft').width()
+            });
             scope.$apply();
         });
     };
@@ -30,6 +36,16 @@ angular.module('ticoBuzzApp')
         angular.element(document).ready(function() {
         	angular.element('#body').removeClass('body-spacing');
         	angular.element('#navbar').removeClass('navbar-fixed-top');
+        });
+    };
+  })
+  .directive('resize', function ($window) {
+    return function(scope) {
+        angular.element($window).bind('resize', function() {
+            angular.element('#sharebar').css({
+                width:  angular.element('#bodyLeft').width()
+            });
+            scope.$apply();
         });
     };
   });
