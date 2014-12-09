9d4bfaa1023cd9d1ee83d914e0de216cc874cd18
Andres <andres_m113@hotmail.com>
2014-12-08 18:16:49 -0600

    Sticky Sharebar
    Ahora el sharebar se queda fixed cuando se hace scroll y el Navbar se
    queda estatico en la parte superior de la pagina

--- app/scripts/controllers/articulo-detail.js
+++ app/scripts/controllers/articulo-detail.js
@@ -9,5 +9,27 @@
  */
 angular.module('ticoBuzzApp')
   .controller('ArticuloDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-    $scope.titulo = $routeParams.titulo.replace(/-/gi, ' ');;
-  }]);
+    $scope.titulo = $routeParams.titulo.replace(/-/gi, ' ');
+  }])
+  .directive('scroll', function ($window) {
+    return function(scope) {
+        angular.element($window).bind('scroll', function() {
+             if (this.pageYOffset >= 220) {
+             	 angular.element('#sharebar').addClass('sharebar sharebar-scrolled');
+                 console.log('Scrolled below header.');
+             } else {
+             	 angular.element('#sharebar').removeClass('sharebar-scrolled');
+                 console.log('Header is in view.');
+             }
+            scope.$apply();
+        });
+    };
+  })
+  .directive('example', function() {
+    return function() {
+        angular.element(document).ready(function() {
+        	angular.element('#body').removeClass('body-spacing');
+        	angular.element('#navbar').removeClass('navbar-fixed-top');
+        });
+    };
+  });
